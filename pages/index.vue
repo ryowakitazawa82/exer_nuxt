f72feb158fe92f623be20289f63020bce165a5e1
ryowa.kitazawa <ryowa.kitazawa@gmail.com>
2021-12-29 18:06:55 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+      <p>{{ message }}</p>
+      <hr>
+      <router-link to="/price">Price page</router-link>
+  </section>
+</template>
+
+
+<script>
+export default {
+  data: function(){
+    return {
+      message: 'こんにちは！'
+    }
+  }
+}
+</script>
